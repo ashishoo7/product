24b293fea894e0239700d596cb527587dac31718
ashish007 <ashish.pnd2@gmail.com>
2024-12-09 22:10:43 +0530

    Selected Variant

--- main.js
+++ main.js
@@ -2,17 +2,20 @@ const app =  Vue.createApp({
     data(){
         return{
             cart:0,
-            product: 'Boots',
-            image: './assets/images/socks_green.jpg',
+            product: 'IPD',
+            brand: 'Vue Mastery For',
+            selectedVarient: 0,
+            // image: './assets/images/socks_green.jpg',
             // url: 'https://github.com/ashishoo7/product',
             // inStock:false
-            inStock: true,
-            onSale: false,
+            // inStock: true,
+            onSale: true,
+            
             details: ['50% Cotton','20% Wool', '30% Polyester'],
             sizes: ['XS: EUR','S USA', 'M INA', 'L PAK', 'XL UK'],
             varients:[
-                {id:224 , color:'blue', image: './assets/images/socks_blue.jpg'},
-                {id:225, color:'green', image: './assets/images/socks_green.jpg '}, 
+                {id:224 , color:'blue', image: './assets/images/socks_blue.jpg', quantity:50},
+                {id:225, color:'green', image: './assets/images/socks_green.jpg ', quantity:0}, 
             ]
         }
     },
@@ -21,8 +24,19 @@ const app =  Vue.createApp({
     addToCart(){
         this.cart +=1;
     },
-    updateImage(varientImage){
-      this.image=varientImage;
+    updateVarient(index){
+      this.selectedVarient=index 
+    }, 
+  },
+  computed:{
+    title(){
+      return this.brand + '' + this.product + '' + this.onSale
+    },
+    image(){
+      return this.varients[this.selectedVarient].image
+    },
+    inStock(){
+      return this.varients[this.selectedVarient].quantity
     }
   }
     })
